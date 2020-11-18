93afbcb1f5440d680b162618bed5407f2661f2a3
Baptiste Lamotte <lamotte.baptiste@gmail.com>
2020-11-18 18:20:04 +0100

    added schema

---
+++ model/experience.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const experienceSchema = mongoose.Schema({
+        
+		startingDate:String,
+		endingDate:String,
+		job:String,
+		firm : String, 
+		city :String
+		    
+})
+
+const experienceModel = mongoose.model('experience', experienceSchema)
+
+module.exports = experienceModel
